d79f515cfc554eee86add60f42c7a433c97645d6
Ákos Kovács <akoskovacs@gmx.com>
2011-11-05 21:12:36 +0100

    Sessions controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :notice => "Welcome!"
+    else
+      redirect_to 'new'
+    end
+  end
+end
