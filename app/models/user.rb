d1c598f3f3559f5057d998dad4247d3a722f8471
Ákos Kovács <akoskovacs@gmx.com>
2011-11-05 19:16:27 +0100

    Validations added to User. Password managed by has_secure_passoword.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :name, :email, :password, :password_confirmation
+  validates :name, :presence => true
+
+  #email_regex = /\A[\w+\-\.]+@[a-z\d\-\.]+\.[a-z]+\z\i/
+  validates :email, :presence => true,
+                    :uniqueness => true,
+                    #:format => { :with => email_regex },
+                    :length => { :minimum => 3 }
 end
