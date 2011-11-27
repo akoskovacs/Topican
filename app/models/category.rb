76cb87e6f273550cb4a27f238a56aa22175354c1
Ákos Kovács <akoskovacs@gmx.com>
2011-11-27 15:26:22 +0100

    Posts now depend on destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,11 @@
 class Category < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, :dependent => :destroy
   belongs_to :user
-
+  
+  attr_accessible :name, :description
   validates :name, :presence => true,
                    :uniqueness => true
   validates :user_id, :presence => true
+
+  default_scope order("created_at DESC")
 end
