fa2942d1c30ff5367369770c65fc91d24d69b700
Ákos Kovács <akoskovacs@gmx.com>
2011-11-27 15:28:06 +0100

    Accessibles for content and category id added to Post model to prevent mass-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  attr_accessible :content, :category_id
+
   validates :content, :presence => true
   validates :user_id, :presence => true
   validates :category_id, :presence => true
