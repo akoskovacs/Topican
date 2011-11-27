0a7f2f18ff64e3b6b69a5896ca19ae240918128e
Ákos Kovács <akoskovacs@gmx.com>
2011-11-27 01:26:07 +0100

    password_digest added to the log filter.

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module Forum
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_digest]
 
     # Enable the asset pipeline
     config.assets.enabled = true
