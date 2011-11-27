521bdfd6ef319a62a6fa4c00404f15cac4188918
Ákos Kovács <akoskovacs@gmx.com>
2011-11-27 01:55:59 +0100

    user_id and category_id validations added.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :category
 
   validates :content, :presence => true
+  validates :user_id, :presence => true
+  validates :category_id, :presence => true
 
   default_scope order("created_at DESC")
 end
